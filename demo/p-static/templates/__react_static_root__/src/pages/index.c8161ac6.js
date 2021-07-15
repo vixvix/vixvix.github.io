878d001b3b52b9bdc642adb548da21e12464e4d6
Vincent Xu <vincent.xu@pointclickcare.com>
2021-07-15 15:08:11 -0400

    adding p-static testing pages

---
+++ demo/p-static/templates/__react_static_root__/src/pages/index.c8161ac6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{56:function(e,t,n){"use strict";n.r(t);var c=n(0),a=n.n(c);t.default=function(){return a.a.createElement("div",{style:{textAlign:"center"}},a.a.createElement("h1",null,"Welcome to React-Static"))}}}]);
